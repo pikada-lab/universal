1363903ddf12b53750f5fdbce91f59c8faf08b6f
Dzhigurda Anton <info@pikada-lab.ru>
2022-02-02 18:54:57 +0300

    init commit aptechki

---
+++ server.ts
@@ -0,0 +1,95 @@
+import 'zone.js/node';
+
+import { ngExpressEngine } from '@nguniversal/express-engine';
+import * as express from 'express';
+import { join } from 'path';
+
+import { AppServerModule } from './src/main.server';
+import { APP_BASE_HREF } from '@angular/common';
+import { existsSync } from 'fs';
+
+// The Express app is exported so that it can be used by serverless Functions.
+export function app() {
+  const server = express();
+  const distFolder = join(process.cwd(), 'dist/browser');
+  const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
+
+  // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
+  server.engine('html', ngExpressEngine({
+    bootstrap: AppServerModule,
+  }));
+
+  server.set('view engine', 'html');
+  server.set('views', distFolder);
+  const heroes = [
+    { id: 11, name: 'Dr Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+  ];
+
+  let getID = () => {
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11; 
+  }
+  // TODO: implement data requests securely
+  server.get('/api/heroes/', (req, res) => { 
+    res.setHeader("Content-Type", "application/json"); 
+    res.send(JSON.stringify(heroes));
+  });
+
+  server.get('/api/heroes/:id', (req, res) => {  
+    const index = heroes.findIndex(r => r.id == +req.params?.id);
+    if(!~ index) { 
+     res.status(404).send('Heroes not found');
+    }
+    res.setHeader("Content-Type", "application/json"); 
+    res.send(JSON.stringify(heroes[index]));
+  });
+  server.delete('/api/heroes/:id', (req, res) => {  
+    const index = heroes.findIndex(r => r.id == +req.params?.id);
+    if(!~ index) { 
+     res.status(404).send('Heroes not found');
+    }
+    res.setHeader("Content-Type", "application/json"); 
+    res.send(JSON.stringify(heroes.splice(index, 1)));
+  });
+  // Serve static files from /browser
+  server.get('*.*', express.static(distFolder, {
+    maxAge: '1y'
+  }));
+
+  // All regular routes use the Universal engine
+  server.get('*', (req, res) => {
+    res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
+  });
+
+  return server;
+}
+
+function run() {
+  const port = process.env['PORT'] || 4000;
+
+  // Start up the Node server
+  const server = app();
+  server.listen(port, () => {
+    console.log(`Node Express server listening on http://localhost:${port}`);
+  });
+}
+
+// Webpack will replace 'require' with '__webpack_require__'
+// '__non_webpack_require__' is a proxy to Node 'require'
+// The below code is to ensure that the server is run only when not requiring the bundle.
+declare const __non_webpack_require__: NodeRequire;
+const mainModule = __non_webpack_require__.main;
+const moduleFilename = mainModule && mainModule.filename || '';
+if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
+  run();
+}
+
+export * from './src/main.server';
